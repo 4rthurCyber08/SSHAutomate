882cdec0c2b9ee13c2d214c3d4942c9f3774b3c6
ArJames Inson <aerin.james820@gmail.com>
2025-11-09 01:46:51 +0800

    Delete Automation directory

--- Automation/Automation Tool Files/manifest.pp
+++
@@ -1,8 +0,0 @@
-node 'cisco.example.com' {
-  cisco_ios_interface { 'GigabitEthernet0/1':
-    ensure => present,
-    description => "Uplink to Core",
-    speed => 'auto',
-    duplex => 'auto',
-    vlan_access => '10',
-}
--- Automation/_Python/Ex 02 - RSTLabs/updatelib.py
+++
@@ -1,5 +0,0 @@
-#Run POWERSHELL as ADMINISTRATOR and enter the commands
-
-    #py -m pip install --upgrade pip
-    #py -m pip install netmiko
-    #py -m pip install cryptography
